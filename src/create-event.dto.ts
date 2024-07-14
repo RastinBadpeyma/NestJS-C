462c91967ccbeb363eaa9589d97b41b6b81c5da0
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2024-07-14 08:23:46 +0330

    Request Payload- Data Transfer Objects

---
+++ src/create-event.dto.ts
@@ -0,0 +1,14 @@
+export class CreateEventDto {
+   name:string;
+   description: string;
+   when: string;
+   address: string;
+}
+
+
+// export interface CreateEventDto {
+//    name:string,
+//    description: string,
+//    when: string,
+//    address: string
+// }
--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { CreateEventDto } from './create-event.dto';
 
 @Controller('/events')
 export class EventsController {
@@ -14,7 +15,7 @@ export class EventsController {
       return {id: 1 , name: 'First event'}
    }
    @Post()
-   create(@Body() input) {
+   create(@Body() input: CreateEventDto) {
       return input;
    }
    @Patch(':id')
