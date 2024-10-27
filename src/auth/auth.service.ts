819be88ab1b7fbaffb27bfead44b0fca92817c7e
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2024-10-27 14:52:07 +0330

    Hashing Passwords with Bcrypt
    ⚫npm install bcrypt
    ⚫npm install --save-dev @types/bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { User } from "./user.entity";
+import * as bcrypt from "bcrypt";
 
 
 @Injectable()
@@ -14,5 +15,9 @@ export class AuthService {
          username:user.username,
          sub:user.id
       });
+   }
+
+   public async hashPassword(password: string): Promise<string> {
+      return await bcrypt.hash(password,10);
    }
 }
